2cd5d150c6cbb92d5a487787fc6e5566ac3f2ed7
Noam Tamim <noam.tamim@kaltura.com>
2018-07-18 15:18:18 +0300

    Update and rename playkitvr/travis-push-to-bintray.sh to travis-push-to-bintray.sh

--- travis-push-to-bintray.sh
+++ travis-push-to-bintray.sh
@@ -27,15 +27,11 @@ if [[ ! $TAG_VERSION_NAME =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
 fi
 
 # Check that defined library version matches the tag.
-if ! grep -q "ext.playkitVersion = '$TAG_VERSION_NAME'" playkit/version.gradle
+if ! grep -q "ext.playkitVersion = '$TAG_VERSION_NAME'" playkitvr/version.gradle
 then
     echo "Library version name in build.gradle does not match tag name; will perform a dry-run."
     DRY_RUN=true
 fi
 
-
-# Assuming a successful playkit:build, create javadoc jar, sources jar, pom
-./gradlew playkit:publishMavenPublicationToMavenLocal -x mavenAndroidJavadocs
-
 # Upload
-./gradlew playkit:bintrayUpload -PdryRun=$DRY_RUN -PbintrayUser=$BINTRAY_USER -PbintrayKey=$BINTRAY_KEY
+./gradlew playkitvr:bintrayUpload -PdryRun=$DRY_RUN -PbintrayUser=$BINTRAY_USER -PbintrayKey=$BINTRAY_KEY
